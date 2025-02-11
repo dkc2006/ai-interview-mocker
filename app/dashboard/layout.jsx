9c546e7edd41d8bfefec358f410005237e722da2
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-02-11 23:11:52 +0530

    Header

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+// import Header from "@/app/dashboard/_components/Header";
+import Header from "./_components/Header"
 function DashboardLayout({children}){
     return (
-        <div>{children}</div>
+        <div>
+            <Header/>
+            {children}</div>
     )
 }
 
